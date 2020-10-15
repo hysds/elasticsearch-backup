bdde867ca3cfde6e7b3a8bd4f865640b14e66ba3
Gerald Manipon <pymonger@gmail.com>
2020-10-15 22:04:04 +0000

    update for ES7

--- backup_all.py
+++ backup_all.py
@@ -13,7 +13,7 @@ def backup(url, backup_root):
     os.makedirs(backup_root)
 
     # get all indices
-    r = requests.get('%s/_aliases' % url, verify=False)
+    r = requests.get('%s/_aliases' % url, verify=False, headers={'content-type': 'application/json'})
     r.raise_for_status()
     res = r.json()
     #print(json.dumps(res, indent=2))
@@ -27,7 +27,7 @@ def backup(url, backup_root):
         if not os.path.isdir(d): os.makedirs(d)
 
         # save settings
-        r = requests.get('%s/%s/_settings' % (url, idx), verify=False)
+        r = requests.get('%s/%s/_settings' % (url, idx), verify=False, headers={'content-type': 'application/json'})
         r.raise_for_status()
         settings = r.json()
         s = os.path.join(d, '%s.settings' % idx)
@@ -36,7 +36,7 @@ def backup(url, backup_root):
         print("Backed up settings for %s" % idx)
 
         # save mapping
-        r = requests.get('%s/%s/_mapping' % (url, idx), verify=False)
+        r = requests.get('%s/%s/_mapping' % (url, idx), verify=False, headers={'content-type': 'application/json'})
         r.raise_for_status()
         mapping = r.json()
         m = os.path.join(d, '%s.mapping' % idx)
@@ -46,28 +46,29 @@ def backup(url, backup_root):
 
         # save docs
         query = {
-            "fields": "_source",
             "query": {
                 "match_all": {}
             }
         }
-        txt = os.path.join(d, '%s.docs' % idx)
-        r = requests.post('%s/%s/_search?search_type=scan&scroll=120m&size=100' % (url, idx), data=json.dumps(query), verify=False)
+        doc_dir = os.path.join(d, 'docs')
+        if not os.path.isdir(doc_dir): os.makedirs(doc_dir)
+        r = requests.post('%s/%s/_search?scroll=120m&size=100' % (url, idx), data=json.dumps(query), verify=False, headers={'content-type': 'application/json'})
         scan_result = r.json()
+        #print("scan_result: {}".format(scan_result))
         count = scan_result['hits']['total']
         scroll_id = scan_result['_scroll_id']
-        docs = []
-        with open(txt, 'w') as f:
-            while True:
-                r = requests.post('%s/_search/scroll?scroll=120m' % url, data=scroll_id, verify=False)
-                if r.status_code != 200:
-                    print("Got status code: {}\n{}".format(r.status_code, r.content))
-                    r.raise_for_status()
-                res = r.json()
-                scroll_id = res.get('_scroll_id', None)
-                if scroll_id is None or len(res['hits']['hits']) == 0: break
-                for hit in res['hits']['hits']:
-                    f.write("%s\n" % json.dumps(hit['_source']))
+        while True:
+            r = requests.post('%s/_search/scroll?scroll=120m' % url, data=json.dumps({'scroll_id': scroll_id}), verify=False, headers={'content-type': 'application/json'})
+            if r.status_code != 200:
+                print("Got status code: {}\n{}".format(r.status_code, r.content))
+                r.raise_for_status()
+            res = r.json()
+            scroll_id = res.get('_scroll_id', None)
+            if scroll_id is None or len(res['hits']['hits']) == 0: break
+            for hit in res['hits']['hits']:
+                txt = os.path.join(doc_dir, '%s.json' % hit['_id'])
+                with open(txt, 'w') as f:
+                    f.write("%s\n" % json.dumps(hit['_source'], sort_keys=True, indent=4))
         #b = os.path.join(d, '%s.docs.bz2' % idx)
         #with bz2.BZ2File(b, 'w') as f:
         #    for doc in docs:
